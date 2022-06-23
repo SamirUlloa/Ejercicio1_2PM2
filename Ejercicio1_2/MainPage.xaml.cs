e8900d861eba2d80f852218ef286c8e48c652b2c
Samir Ulloa <samir.ulloa@yahoo.es>
2022-06-22 20:56:40 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public MainPage(DateTime date)
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Empleado
+            {
+                nombre = txtNombre.Text,
+                apellido = txtApellido.Text,
+                edad = txtEdad.Text,
+                correo = txtCorreo.Text
+            };
+
+            var segunda = new SegundaPage();
+            segunda.BindingContext = emple;
+
+            await Navigation.PushAsync(segunda);
+        }
+    }
+}
---
+++ Ejercicio1_2/Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Models
+{
+    public class Empleado
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public String edad { get; set; }
+        public String correo { get; set; }
+
+    }
+}
